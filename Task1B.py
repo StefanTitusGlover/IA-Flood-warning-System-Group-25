4f6bce225d80623efeed9e2db842aa0cca1b37f1
Stefan Titus-Glover <svnkt2@cam.ac.uk>
2022-01-30 16:15:28 +0000

    Added comments for Task 1B

--- Task1B.py
+++ Task1B.py
@@ -2,8 +2,8 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_by_distance
 
 stations = build_station_list()  # Build list of stations
-location = (52.2053, 0.1218)
-orderedlist = stations_by_distance(stations, location)
+location = (52.2053, 0.1218) # Chose a location by its latitude and longitude
+orderedlist = stations_by_distance(stations, location) # generates an ordered list of stations by distance to the given location
 
-print(orderedlist[:10])
-print(orderedlist[-10:])
+print(orderedlist[:10]) # print the first 10 items of the list
+print(orderedlist[-10:]) # print the last 10 items of the list
