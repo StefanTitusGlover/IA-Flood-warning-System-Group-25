8d8cc1c905a8464d533dc3ecdd6ccdce9cb3ac0a
Stefan Titus-Glover <svnkt2@cam.ac.uk>
2022-01-30 10:08:48 +0000

    Added comments

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -11,12 +11,12 @@ from haversine import haversine, Unit # Imports the haversine function for calcu
 
 def stations_by_distance(stations,p):
     """" Function that  orders a list of stations by distance to a given location"""
-    stationslist = []
+    stationslist = [] # creates an empty list which will be used to store the required station data
     for station in stations:
-        distance = haversine(station.coord,p)
-        temp = [station.name, station.town, distance]
-        stationslist.append(temp)
-    orderedstations = sorted_by_key(stationslist, 2)
+        distance = haversine(station.coord,p) # calculates the distance between a station and the coordinate p
+        temp = [station.name, station.town, distance] # creates a list of the station name, town and distance to a point
+        stationslist.append(temp) # adds the individual list to the station list
+    orderedstations = sorted_by_key(stationslist, 2) # generates an ordered list by distance, the third entry
     return orderedstations
   
 
