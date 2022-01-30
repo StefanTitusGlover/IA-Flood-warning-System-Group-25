210c834fbb643b945845fabfcd6b1976a057231c
Stefan Titus-Glover <svnkt2@cam.ac.uk>
2022-01-30 17:33:33 +0000

    Task 1F completed

--- Task1F.py
+++ Task1F.py
@@ -3,5 +3,12 @@ from floodsystem.stationdata import build_station_list
 stations = build_station_list()
 inconsistentstations = []
 
+for station in stations:
+    check = station.typical_range_consistent()
+    if check == False:
+        inconsistentstations.append(station.name)
+inconsistentstations.sort()
 
+print("The stations with inconsistent water level ranges are ", inconsistentstations)
+print(" The number of stations with inconsistent water level ranges is", len(inconsistentstations))
 
