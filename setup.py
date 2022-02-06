fa5a877ac8703d7869349709109b50b000c6bab8
Lottie Butler <cmb251@cam.ac.uk>
2022-02-06 13:53:47 +0000

    -

--- setup.py
+++ setup.py
@@ -4,3 +4,7 @@ setup(name='floodsystem',
       description='CUED Part IA flood warning system exercise',
       packages=['floodsystem'],
       )
+
+# pip install requests - How to install extra modules
+# pip install python-dateutil - How to install extra modules
+# IA-Flood-warning-System-Group-25 name of file
