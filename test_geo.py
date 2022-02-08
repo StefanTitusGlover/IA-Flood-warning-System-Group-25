d41e041bad289e701d25a76d7100769e3aa92b3f
Lottie Butler <cmb251@cam.ac.uk>
2022-02-08 01:54:44 +0000

    All tests apart from haversine and test station within r functionality

--- test_geo.py
+++ test_geo.py
@@ -1,13 +1,93 @@
-from floodsystem.geo import rivers_with_station, stations_by_distance, stations_within_radius
+from distutils.command.build import build
 from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_by_distance
+from floodsystem.geo import stations_within_radius
+from floodsystem.geo import rivers_with_station
+from floodsystem.geo import stations_by_river
+from floodsystem.geo import rivers_by_station_number
 
-def test_stations_by_distance(): # Testing the stations_by_distance function
+def test_stations_by_distance_types(): # Testing the stations_by_distance function
     stationlist = build_station_list() # builds a list of all the stations
     location = (52.2053, 0.1218)
     stations = stations_by_distance(stationlist,location)
     example = stations[0]
+    assert type(stations) == list
     assert type(example) == tuple
     assert type(example[0]) == str
     assert type(example[1]) == str
     assert type(example[2]) == float
+
+def test_stations_by_distance_functionality(): #checks function ouputs stations ordered in ascending dist
+    stationlist = build_station_list()
+    location =  (52.2053, 0.1218)
+    stations = stations_by_distance(stationlist, location)
+    i = 0
+    while i < (len(stations) - 1):
+        assert stations[i][2] <= stations[i+1][2] #each station should have smaller dist than the next in the list
+        i += 1
+
+def test_stations_within_radius_types(): 
+    stationlist = build_station_list()
+    centre = (52.305, 0.1218) #coordinates for centre of cambridge
+    r = 10
+    stations = stations_within_radius(stationlist, centre, 10)
+    assert type(stations) == list
+    i = 0
+    while i < len(stations):
+        assert type(stations[i]) == str 
+        i += 1
+    
+    
+
+def test_rivers_with_stations():
+    stationlist = build_station_list()
+    rivers = rivers_with_station(stationlist)
+    assert type(rivers) == list
+    i = 0
+    while i < len(rivers):
+        assert type(rivers[i]) == str
+        i += 1
+    river_set = set(rivers)  #set assures there are no duplicates
+    assert len(river_set) == len(rivers) #checks there are no duplicates
+
+def test_stations_by_river():
+    stationlist = build_station_list()
+    rivers = rivers_with_station(stationlist) #list of rivers
+    rivers.sort()
+    river_dict = stations_by_river(stationlist)
+    assert type(river_dict) == dict #ensures output of function is a dictionary
+    river_keys = list(river_dict.keys())
+    i = 0
+    total = 0
+    while i < (len(river_keys)):
+        for river in rivers:
+            if river_keys[i] == rivers: #checks if all entries in river_keys are in list of rivers
+                total += 1
+        i += 1
+    assert total == len(river_keys)
+
+def test_rivers_by_station_number():
+    stationlist = build_station_list()
+    N = 10
+    rivs_most_stations = rivers_by_station_number(stationlist, N)
+    assert type(rivs_most_stations) == list
+    example = rivs_most_stations[0]
+    assert type(example) == tuple
+    assert type(example[0]) == str
+    assert type(example[1]) == int
+    assert (len(rivs_most_stations) >= N) #function should return list with >= N entries. Accounts for the case where multiple rivers have Nth largest no. stations
+    i = 0
+    while i < (len(rivs_most_stations) - 1):
+        assert rivs_most_stations[i][1] >= rivs_most_stations[i+1][1] #should return tuples in decr order
+        i += 1
+    
+test_stations_by_distance_types()
+test_stations_by_distance_functionality()
+test_stations_within_radius_types()
+
+test_rivers_with_stations()
+test_stations_by_river()
+test_rivers_by_station_number()
+
+#note to try and save
 
