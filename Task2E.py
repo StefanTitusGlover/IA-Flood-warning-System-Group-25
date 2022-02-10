25f0aebed09f39f9f43b2142439f13c05ccc1f20
Stefan Titus-Glover <svnkt2@cam.ac.uk>
2022-02-10 17:53:52 +0000

    The functions for 2E are complete but the task requires code that will be made in Tasks 2B and C

--- Task2E.py
+++ Task2E.py
@@ -8,3 +8,5 @@ def run():
 if __name__ == "__main__":
     print("*** Task 2E: CUED Part IA Flood Warning System ***")
     run()
+
+# 2E is not currently finished and required code from 2B and C to be done
